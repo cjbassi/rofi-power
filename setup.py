b4ad893d77445e3bbf6a1f382804c162d2e11b39
Caleb Bassi <calebjbassi@gmail.com>
2018-11-10 16:17:47 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='rofi-power',
+    version='0.1.0',
+    scripts=['rofi-power'],
+    url='https://github.com/cjbassi/rofi-power',
+)
