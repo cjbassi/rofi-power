4fe3c8a19be5f6aedc5f7bf44a59fd1ed3762bd4
Caleb Bassi <calebjbassi@gmail.com>
2018-11-11 09:46:15 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rofi-power',
-    version='0.2.0',
+    version='0.3.0',
     scripts=['rofi-power'],
     url='https://github.com/cjbassi/rofi-power',
 )
